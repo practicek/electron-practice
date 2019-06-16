35fd905f6e43685407367c8cc6be458edd45bb3c
Your Name <you@example.com>
2019-06-17 00:33:44 +0900

    fix main.js indent

--- main.js
+++ main.js
@@ -1,45 +1,57 @@
+
+//load electron module
 const electron = require("electron");
+
+//load app fron electron
 const app = electron.app;
+
+//create browser object from electron
 const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
  
-const {ipcMain} = require('electron');
+//set object for ipc
+const {ipcMain} = require("electron");
  
-// 準備ができたタイミングで呼ばれるイベント
-app.on('ready',function(){
-    // メインウィンドウを作成
-    mainWindow = new BrowserWindow({"webPreferences": {"nodeIntegration":true},"width":800,"height":600});
-    mainWindow.loadURL('file://' + __dirname + '/html/index.html');
-    mainWindow.on('closed',function(){
-        mainWindow = null;
+//init event when process starts
+//fork renderer process and load html
+app.on("ready",function(){
+  //create main window with width and height
+  mainWindow = new BrowserWindow({"webPreferences": {"nodeIntegration":true},"width":800,"height":600});
+
+  //load first html file from local
+  mainWindow.loadURL("file://"+ __dirname +"/html/index.html");
+
+  //behavior when window closed
+  mainWindow.on("closed",function(){
+    mainWindow = null;
     });
 });
  
-// 同期メッセージの受信
-ipcMain.on('mul-sync', (event, arg) => {
-    console.log(arg);
-    // レンダラープロセスへ返信
-    event.returnValue = arg.a + arg.b;
+//get message from renderer process
+ipcMain.on("mul-sync",(event, arg) => {
+  console.log(arg);
+
+  //send result to renderer process sync
+  event.returnValue = arg.a + arg.b;
 });
  
-// 非同期メッセージの受信
+//get message from renderer process
 ipcMain.on('mul-async', (event, arg) => {
-    console.log(arg);
-    // レンダラープロセスへ返信
-    var result = arg.a + arg.b;
-    event.sender.send('mul-async-reply',result);
+  console.log(arg);
+
+  var result = arg.a + arg.b;
+  
+  //send result to renderer process async
+  event.sender.send('mul-async-reply',result);
 });
 
 function searchPrime(x){
-        var flag=true;
-
+  var flag=true;
   for (var i=2; i<x/2; i++){
     if (x%i==0){
       flag=false;
       break;
-
     }
-
   }
   console.log(flag);
   return flag
